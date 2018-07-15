31feb4540b994764508485fc96a27ccaa53ccbfe
lduran2 <29983534+lduran2@users.noreply.github.com>
2018-07-15 00:15:37 -0400

    Update GatewayDevice.cs

--- src/main/cs/GatewayDevice.cs
+++ src/main/cs/GatewayDevice.cs
@@ -1,15 +1,49 @@
+using System; /* String */
 using System.Net; /* IPAddress */
 
 namespace org {
 	namespace dark_archives {
-		namespace NetUPnP {
+ 		namespace NetUPnP {
 			public sealed class GatewayDevice {
-				// GateDiscover
-				public object getFriendlyName() { return null; }
-				public object getPresentationURL() { return null; }
-				public object getModelName() { return null; }
-				public object getModelNumber() { return null; }
-				public IPAddress getLocalAddress() { return new IPAddress(0); }
+
+				public GatewayDevice() {
+					this.friendlyName = "";
+					this.presentationUrl = "";
+					this.modelName = "";
+					this.modelNumber = "";
+					this.localAddress = new IPAddress(0);
+				}
+
+				public boolean isConnected() {
+					string connectionStatus;
+
+					connectionStatus = simpleUpnpCommand(controlUrl, serviceType, "GetStatusInfo", null);
+					return (
+						(connectionStatus != null)
+						&& connectionStatus.EqualsIgnoreCase("Connected")
+					);
+				}
+
+				private IDictionary<string, string> simpleUpnpCommand(string url, string service, string action, IDictionary<string, string> args) {
+					return new Dictionar<string, string>();
+				}
+
+				public string Format(string format) {
+					return String.Format(format,
+					                     this.friendlyName,
+					                     this.presentationUrl,
+					                     this.modelName,
+					                     this.modelNumber,
+					                     this.localAddress.ToString()
+					);
+				}
+
+				private readonly string friendlyName;
+				private readonly string presentationUrl;
+				private readonly string modelName;
+				private readonly string modelNumber;
+				private readonly IPAddress localAddress;
+
 			}
 		} /* end namespace org.dark_archives.NetUPnP */
 	} /* end namespace org.dark_archives */
