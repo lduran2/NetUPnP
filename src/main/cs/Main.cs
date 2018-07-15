780fbc953d7831cb7489b9fed78e1c51241563ec
lduran2 <29983534+lduran2@users.noreply.github.com>
2018-07-15 00:14:49 -0400

    Update Main.cs

--- src/main/cs/Main.cs
+++ src/main/cs/Main.cs
@@ -5,6 +5,7 @@ using org.dark_archives.NetUPnP;
 
 public static class MainClass {
 	public static void Main(params string[] args) {
+		GatewayDevice active;
 		GatewayDiscover gatewayDiscover;
 		IDictionary<IPAddress, GatewayDevice> gateways;
 		IEnumerator<GatewayDevice> devices;
@@ -23,28 +24,41 @@ public static class MainClass {
 			return;
 		}
 
-		addLogLine(String.Format("{0} gateway{1} found\n",
-		                         gateways.Count,
-		                         ((gateways.Count > 0) ? "s" : "")));
+		addLogLine(
+			String.Format("{0} gateway{1} found\n",
+				gateways.Count,
+				((gateways.Count > 0) ? "s" : "")
+			)
+		);
 
 		devices = gateways.Values.GetEnumerator();
 		for (k = 1; (gateways.Count >= k); ++k) {
 			devices.MoveNext();
 			dev = devices.Current;
-			addLogLine(String.Format("Listing gateway details of device "
-			                         	+ "#{0}\n"
-			                         	+ "\tFriendly name: {1}\n"
-			                         	+ "\tPresentation URL: {2}\n"
-			                         	+ "\tModel name: {3}\n"
-			                         	+ "\tModel number: {4}\n"
-			                         	+ "\tLocal interface address: "
-			                         	+ "{5}\n",
-			                         k, dev.getFriendlyName(),
-			                         dev.getPresentationURL(),
-			                         dev.getModelName(),
-			                         dev.getModelNumber(),
-			                         dev.getLocalAddress().ToString()
-			));
+			addLogLine(
+				dev.Format(
+					String.Format(
+						"Listing gateway details of device #{0}\n"
+						+ "\tFriendly name: {{0}}\n"
+						+ "\tPresentation URL: {{1}}\n"
+						+ "\tModel name: {{2}}\n"
+						+ "\tModel number: {{3}}\n"
+						+ "\tLocal interface address: {{4}}\n",
+						k
+					)
+				)
+			);
+		}
+
+		/* choose the first active gateway to test */
+		active = gatewayDiscover.ValidGateway(null);
+
+		if (null != active) {
+			addLogLine(active.Format("Using gateway: {0}"));
+		}
+		else {
+			addLogLine("No active gateway device found");
+			addLogLine("Stopping weupnp");
 		}
 	}
 
